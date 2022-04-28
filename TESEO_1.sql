a5705da3509421a93165ebeb0ad161de9727d212
Alejandro <>
2022-04-28 01:43:44 -0500

    Actualización de consulta base

--- TESEO_1.sql
+++ TESEO_1.sql
@@ -1,12 +1,16 @@
 -- Construcción de Ficha y Ponderación --
 --declare
 --begin
-    select wh.work_header_id, wh.cod_sgae, dc1.doc_date, wc.start_effectivity_date, 
+    select wh.work_header_id, wh.cod_sgae, wh.work_type_id, dc1.doc_date, wc.start_effectivity_date, 
     dc.percentage_d, dc.percentage_f, dc.percentage_l, dc.percentage_m,
     cmp.percentage, cmp.length, cs1.composition_length, cs1.percentage,
-    w.distribution_typology_id, w.nationality_id, w.work_length, w.music_length,
-    e.work_length, e.music_length, v.version_id,
-    case when 
+    w.distribution_typology_id, w.nationality_id, w.work_length w_work_length,
+    w.music_length w_music_length, e.work_length w_work_length, e.music_length e_music_length, 
+    v.version_id,
+    case when dc.contract_header_id is null and dc1.contract_header_id is null 
+    then 1
+    else 0 
+    end contrato_default
     from t_aud.work_header wh 
     left join t_aud.composition_cmp cmp on cmp.work_header_id = wh.work_header_id
     left join t_aud.cue_sheet cs on cs.work_header_id = wh.work_header_id
@@ -20,6 +24,6 @@
     left join t_aud.version v on v.work_header_id = wh.work_header_id
     where  wc.start_effectivity_date between to_date('2021-01-01','yyyy-mm-dd') 
     and to_date('2022-01-02','yyyy-mm-dd') and dc.distribution_key_id = 2
-    and rownum <= 100 
-    order by wc.start_effectivity_date desc;
+    and rownum <= 1000 
+    order by contrato_default desc, wc.start_effectivity_date desc;
 -- end;
