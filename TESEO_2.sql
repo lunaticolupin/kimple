ba7a5a1249acceb5ab6d82e3f892d5afbfc5a591
Alejandro <lunaticolupin@outlook.com>
2022-05-31 08:40:11 -0500

    actualiza script

--- TESEO_2.sql
+++ TESEO_2.sql
@@ -1,10 +1,197 @@
 
-select * 
+SELECT COD_SGAE, wh.WORK_TYPE_ID,  wc.START_EFFECTIVITY_DATE,  ch.contract_type_id, dc.COD_IPI, dc.DECLARATION_CONTRACT_ID, dc.PROFESSION_ID, 
+dc.PERCENTAGE porcentaje_contrato, dc2.DISTRIBUTION_KEY_ID, nvl(dc2.PERCENTAGE_D, 25) percentage_d, dc2.PERCENTAGE_f, nvl(dc2.PERCENTAGE_L, 50) percentage_l,
+nvl(dc2.PERCENTAGE_M, 25) percentaje_m
 from t_aud.work_header wh 
 join t_aud.work_contract wc on wc.work_header_id = wh.work_header_id 
 join t_aud.contract_header ch on ch.contract_header_id = wc.contract_header_id 
-left join t_aud.distribution_contract dc on dc.contract_header_id = ch.contract_header_id
-left join t_aud.declaration_contract dc1 on dc1.contract_header_id = ch.contract_header_id
-left join t_aud.work w on w.work_header_id = wh.work_header_id
-where wh.cod_sgae= '50137347' and wc.start_effectivity_date
-< to_date('2021-03-01','yyyy-mm-dd');
+LEFT JOIN T_AUD.DECLARATION_CONTRACT dc ON dc.CONTRACT_HEADER_ID = ch.CONTRACT_HEADER_ID 
+LEFT JOIN T_AUD.DISTRIBUTION_CONTRACT dc2 ON dc2.CONTRACT_HEADER_ID = ch.CONTRACT_HEADER_ID  
+--LEFT join t_aud.cue_sheet cs on cs.work_header_id = wh.work_header_id AND wh.WORK_TYPE_ID IN (1,2)
+where wh.cod_sgae= '50164403' and wc.START_EFFECTIVITY_DATE < to_date('2021-03-31','yyyy-mm-dd')-- and wc.start_effectivity_date BETWEEN to_date('2020-09-01','yyyy-mm-dd') and to_date('2021-03-31','yyyy-mm-dd')
+-- GROUP BY COD_SGAE, wh.WORK_TYPE_ID, wc.START_EFFECTIVITY_DATE, ch.CONTRACT_TYPE_ID, dc.COD_IPI, dc.DISTRIBUTION_PART_ID, dc2.DISTRIBUTION_KEY_ID , dc2.PERCENTAGE_D ,
+-- dc2.PERCENTAGE_D, dc2.PERCENTAGE_F, dc2.PERCENTAGE_L, dc2.PERCENTAGE_M;  
+
+SELECT wh.COD_SGAE, wh.WORK_HEADER_ID, dc.CONTRACT_HEADER_ID, wh.WORK_TYPE_ID, dc.DECLARATION_CONTRACT_ID, 
+nvl(w2.DISTRIBUTION_TYPOLOGY_ID, w.DISTRIBUTION_TYPOLOGY_ID) tipologia,  nvl(w2.NATIONALITY_ID, w.NATIONALITY_ID) nacionalidad , 
+wc.START_EFFECTIVITY_DATE,  ch.contract_type_id, dc.COD_IPI, dc.DECLARATION_CONTRACT_ID, dc.PROFESSION_ID, dc2.DISTRIBUTION_KEY_ID, 
+dc.PERCENTAGE porcentaje_contrato, nvl(dc2.PERCENTAGE_D, 25) porcentaje_d, nvl(dc2.PERCENTAGE_L, 50) porcentaje_l, nvl(dc2.PERCENTAGE_M, 25) porcentaje_m
+from t_aud.work_header wh 
+join t_aud.work_contract wc on wc.work_header_id = wh.work_header_id 
+join t_aud.contract_header ch on ch.contract_header_id = wc.contract_header_id 
+LEFT JOIN T_AUD.DECLARATION_CONTRACT dc ON dc.CONTRACT_HEADER_ID = ch.CONTRACT_HEADER_ID AND ch.CONTRACT_TYPE_ID = 2-- AND dc.START_EFFECTIVITY_DATE BETWEEN to_date('2021-07-01','yyyy-mm-dd') and to_date('2021-09-30','yyyy-mm-dd')
+LEFT JOIN T_AUD.DISTRIBUTION_CONTRACT dc2 ON dc2.CONTRACT_HEADER_ID = ch.CONTRACT_HEADER_ID AND ch.CONTRACT_TYPE_ID = 1-- AND dc.START_EFFECTIVITY_DATE BETWEEN to_date('2021-07-01','yyyy-mm-dd') and to_date('2021-09-30','yyyy-mm-dd')
+LEFT JOIN T_AUD.EPISODE e ON e.WORK_HEADER_ID = wh.WORK_HEADER_ID AND wh.WORK_TYPE_ID = 3 AND e.START_EFFECTIVITY_DATE < to_date('2021-08-18','yyyy-mm-dd')
+LEFT JOIN T_AUD.WORK_HEADER wh3 ON wh3.WORK_HEADER_ID = e.SERIES_HEADER_ID AND wh.WORK_TYPE_ID = 3
+LEFT JOIN T_AUD."WORK" w ON w.WORK_HEADER_ID = wh3.WORK_HEADER_ID AND wh.WORK_TYPE_ID = 3
+LEFT JOIN T_AUD."WORK" w2 ON w2.WORK_HEADER_ID = wh.WORK_HEADER_ID AND wh.WORK_TYPE_ID = 1
+where wh.cod_sgae= :COD_SGAE  and wc.START_EFFECTIVITY_DATE = (
+SELECT max(wc.START_EFFECTIVITY_DATE)  FROM T_AUD.WORK_HEADER wh2
+JOIN T_AUD.WORK_CONTRACT wc ON wc.WORK_HEADER_ID = wh2.WORK_HEADER_ID 
+WHERE wh2.cod_sgae = wh.COD_SGAE  and wc.START_EFFECTIVITY_DATE < to_date('2021-08-18','yyyy-mm-dd'))
+ORDER BY wc.START_EFFECTIVITY_DATE DESC;
+
+SELECT wh.COD_SGAE, wh.WORK_HEADER_ID, dc.CONTRACT_HEADER_ID, wh.WORK_TYPE_ID, 
+nvl(w2.DISTRIBUTION_TYPOLOGY_ID, w.DISTRIBUTION_TYPOLOGY_ID) tipologia,  
+nvl(w2.NATIONALITY_ID, w.NATIONALITY_ID) nacionalidad, wc.START_EFFECTIVITY_DATE,  ch.contract_type_id, 
+dc.COD_IPI, dc.DECLARATION_CONTRACT_ID, dc.PROFESSION_ID, dc2.DISTRIBUTION_KEY_ID, 
+dc.PERCENTAGE porcentaje_contrato, nvl(dc2.PERCENTAGE_D, 25) porcentaje_d, 
+nvl(dc2.PERCENTAGE_L, 50) porcentaje_l, nvl(dc2.PERCENTAGE_M, 25) porcentaje_m
+from t_aud.work_header wh 
+join t_aud.work_contract wc on wc.work_header_id = wh.work_header_id 
+join t_aud.contract_header ch on ch.contract_header_id = wc.contract_header_id 
+LEFT JOIN T_AUD.DECLARATION_CONTRACT dc ON dc.CONTRACT_HEADER_ID = ch.CONTRACT_HEADER_ID 
+LEFT JOIN T_AUD.DISTRIBUTION_CONTRACT dc2 ON dc2.CONTRACT_HEADER_ID = ch.CONTRACT_HEADER_ID 
+LEFT JOIN T_AUD.EPISODE e ON e.WORK_HEADER_ID = wh.WORK_HEADER_ID AND wh.WORK_TYPE_ID = 3 
+	AND e.START_EFFECTIVITY_DATE < to_date('2021-08-18','yyyy-mm-dd')
+LEFT JOIN T_AUD.WORK_HEADER wh3 ON wh3.WORK_HEADER_ID = e.SERIES_HEADER_ID AND wh.WORK_TYPE_ID = 3
+LEFT JOIN T_AUD."WORK" w ON w.WORK_HEADER_ID = wh3.WORK_HEADER_ID AND wh.WORK_TYPE_ID = 3
+LEFT JOIN T_AUD."WORK" w2 ON w2.WORK_HEADER_ID = wh.WORK_HEADER_ID AND wh.WORK_TYPE_ID = 1
+where wh.cod_sgae= :COD_SGAE and wc.START_EFFECTIVITY_DATE = (
+SELECT max(wc.START_EFFECTIVITY_DATE) FROM T_AUD.WORK_HEADER wh2
+JOIN T_AUD.WORK_CONTRACT wc ON wc.WORK_HEADER_ID = wh2.WORK_HEADER_ID 
+WHERE wh2.cod_sgae = wh.COD_SGAE  and wc.START_EFFECTIVITY_DATE < to_date('2021-08-18','yyyy-mm-dd'));
+
+SELECT wh.COD_SGAE, wh.WORK_HEADER_ID, dc.CONTRACT_HEADER_ID, wh.WORK_TYPE_ID, 
+nvl(w2.DISTRIBUTION_TYPOLOGY_ID, w.DISTRIBUTION_TYPOLOGY_ID) tipologia,  
+nvl(w2.NATIONALITY_ID, w.NATIONALITY_ID) nacionalidad, wc.START_EFFECTIVITY_DATE,  ch.contract_type_id, 
+dc.COD_IPI, dc.DECLARATION_CONTRACT_ID, dc.PROFESSION_ID, dc2.DISTRIBUTION_KEY_ID, 
+dc.PERCENTAGE porcentaje_contrato, nvl(dc2.PERCENTAGE_D, 25) porcentaje_d, 
+nvl(dc2.PERCENTAGE_L, 50) porcentaje_l, nvl(dc2.PERCENTAGE_M, 25) porcentaje_m
+from t_aud.work_header wh 
+join t_aud.work_contract wc on wc.work_header_id = wh.work_header_id 
+join t_aud.contract_header ch on ch.contract_header_id = wc.contract_header_id 
+LEFT JOIN T_AUD.DECLARATION_CONTRACT dc ON dc.CONTRACT_HEADER_ID = ch.CONTRACT_HEADER_ID 
+LEFT JOIN T_AUD.DISTRIBUTION_CONTRACT dc2 ON dc2.CONTRACT_HEADER_ID = ch.CONTRACT_HEADER_ID 
+LEFT JOIN T_AUD.EPISODE e ON e.WORK_HEADER_ID = wh.WORK_HEADER_ID AND wh.WORK_TYPE_ID = 3 
+	AND e.START_EFFECTIVITY_DATE < to_date('2021-08-18','yyyy-mm-dd')
+LEFT JOIN T_AUD.WORK_HEADER wh3 ON wh3.WORK_HEADER_ID = e.SERIES_HEADER_ID AND wh.WORK_TYPE_ID = 3
+LEFT JOIN T_AUD."WORK" w ON w.WORK_HEADER_ID = wh3.WORK_HEADER_ID AND wh.WORK_TYPE_ID = 3
+LEFT JOIN T_AUD."WORK" w2 ON w2.WORK_HEADER_ID = wh.WORK_HEADER_ID AND wh.WORK_TYPE_ID = 1
+where wh.cod_sgae= :COD_SGAE and wc.START_EFFECTIVITY_DATE < to_date('2021-08-18','yyyy-mm-dd');
+
+SELECT DISTINCT  wh.COD_SGAE, wh.WORK_HEADER_ID, dc.CONTRACT_HEADER_ID, wh.WORK_TYPE_ID, dc3.VERSION, w.DISTRIBUTION_TYPOLOGY_ID, 
+w.NATIONALITY_ID, wc.START_EFFECTIVITY_DATE,  ch.contract_type_id, 
+dc.COD_IPI, dc.DECLARATION_CONTRACT_ID, dc.PROFESSION_ID, nvl(dc2.DISTRIBUTION_KEY_ID, dc3.DISTRIBUTION_KEY_ID) distribution_key_id,  dc.DISTRIBUTION_PART_ID, 
+dc.PERCENTAGE, nvl(dc2.PERCENTAGE_D, dc3.PERCENTAGE_D) porcentaje_d, nvl(dc2.PERCENTAGE_L, dc3.PERCENTAGE_L) porcentaje_l, nvl(dc2.PERCENTAGE_M, dc3.PERCENTAGE_M) porcentaje_m  
+from t_aud.work_header wh 
+join t_aud.work_contract wc on wc.work_header_id = wh.work_header_id 
+join t_aud.contract_header ch on ch.contract_header_id = wc.contract_header_id 
+LEFT JOIN T_AUD.DECLARATION_CONTRACT dc ON dc.CONTRACT_HEADER_ID = ch.CONTRACT_HEADER_ID AND dc.START_EFFECTIVITY_DATE <  to_date('2021-08-18','yyyy-mm-dd') 
+	AND (dc.END_EFFECTIVITY_DATE IS NULL OR dc.END_EFFECTIVITY_DATE > to_date('2021-08-18','yyyy-mm-dd'))
+LEFT JOIN T_AUD.DISTRIBUTION_CONTRACT dc2 ON dc2.CONTRACT_HEADER_ID = ch.CONTRACT_HEADER_ID AND dc2.START_EFFECTIVITY_DATE < to_date('2021-08-18','yyyy-mm-dd') 
+	AND (dc2.END_EFFECTIVITY_DATE IS NULL OR dc2.END_EFFECTIVITY_DATE > to_date('2021-08-18','yyyy-mm-dd')) AND dc2.DISTRIBUTION_KEY_ID = 2
+LEFT JOIN T_AUD."WORK" w ON w.WORK_HEADER_ID = wh.WORK_HEADER_ID 
+	OR (w.WORK_HEADER_ID in (SELECT e.SERIES_HEADER_ID FROM T_AUD.EPISODE e WHERE e.WORK_HEADER_ID=wh.WORK_HEADER_ID) AND wh.WORK_TYPE_ID IN (2,3))
+	OR (w.WORK_HEADER_ID IN (SELECT v.ORIGINAL_WORK_HEADER_ID  FROM T_AUD.VERSION v WHERE v.WORK_HEADER_ID=wh.WORK_HEADER_ID) AND wh.WORK_TYPE_ID = 4)
+	OR (w.WORK_HEADER_ID IN (SELECT v3.ORIGINAL_WORK_HEADER_ID  FROM T_AUD.VERSION v2, T_AUD.VERSION v3 WHERE v2.WORK_HEADER_ID=wh.WORK_HEADER_ID 
+		AND v2.SERIES_HEADER_ID=v3.WORK_HEADER_ID) AND wh.WORK_TYPE_ID IN (5,6))
+	AND w.START_EFFECTIVITY_DATE < to_date('2021-08-18','yyyy-mm-dd') AND (w.END_EFFECTIVITY_DATE IS NULL OR to_date('2021-08-18','yyyy-mm-dd') < w.END_EFFECTIVITY_DATE)
+LEFT JOIN T_AUD.DEFAULT_CONTRACT dc3 ON dc3.VERSION = CASE WHEN wh.WORK_TYPE_ID IN (1,2,3) THEN 'O' ELSE 'V' END AND dc3.DISTRIBUTION_TYPOLOGY_ID = w.DISTRIBUTION_TYPOLOGY_ID 
+	AND dc3.NATIONALITY_ID IN (-1, w.NATIONALITY_ID) AND dc3.START_DISTRIBUTION_DATE  < to_date('2021-08-18','yyyy-mm-dd') and dc3.DISTRIBUTION_KEY_ID = 2 
+	and dc3.START_EFFECTIVITY_DATE < to_date('2021-08-18','yyyy-mm-dd') 
+	and (dc3.END_EFFECTIVITY_DATE is null or to_date('2021-08-18','yyyy-mm-dd') < dc3.END_EFFECTIVITY_DATE)
+	and dc3.START_DISTRIBUTION_DATE  < to_date('2021-08-18','yyyy-mm-dd')  
+	AND (dc3.END_DISTRIBUTION_DATE IS NULL OR to_date('2021-08-18','yyyy-mm-dd') < dc3.END_DISTRIBUTION_DATE) 
+	AND dc3.ASSOCIATION_ID = 1
+where wh.cod_sgae= :COD_SGAE and wc.START_EFFECTIVITY_DATE < to_date('2021-08-18','yyyy-mm-dd');
+
+SELECT e.SERIES_HEADER_ID FROM T_AUD.EPISODE e WHERE e.WORK_HEADER_ID = 1260345;
+
+SELECT max(wc.START_EFFECTIVITY_DATE)  FROM T_AUD.WORK_HEADER wh2
+JOIN T_AUD.WORK_CONTRACT wc ON wc.WORK_HEADER_ID = wh2.WORK_HEADER_ID 
+WHERE wh2.cod_sgae = '50137347' and wc.START_EFFECTIVITY_DATE < to_date('2021-08-18','yyyy-mm-dd'); 
+
+
+SELECT
+    *
+FROM
+    (
+        SELECT DISTINCT
+            t1.ip2_ipn_id AS ip_name_id,
+            t3.ip2_ipb_id AS ip_base,
+            t2.ip2_ipb_member_code AS ip_member_code,
+            t2.ip2_ipb_code AS ip_base_code,
+            t1.ip2_ipn_name
+            || ' '
+            || t1.ip2_ipn_first_name AS indexed_name,
+            (
+                CASE
+                    WHEN t1.ip2_nsr_id = 2
+                         AND t1.ip2_ipn_internal = 1 THEN 1
+                    ELSE 0
+                END
+            ) AS internal,
+            t2.ip2_ipb_int_role AS role,
+            nvl(lpad(t41.ip2_mbr_soc_code,3,'0'),'099') AS soc_code,
+            t41.ip2_mbr_cc_code AS cc_code,
+            t41.ip2_mbr_rl_code,
+            t41.ip2_mbr_rg_code AS rg_code,
+            t41.ip2_mbr_dt_from,
+            t41.ip2_mbr_dt_to,
+            t41.ip2_mbr_ter_id,
+            t1.ip2_nty_id
+        FROM
+            t_ip2_ip_base_memberships t41,
+            t_ip2_ip_names t1,
+            t_ip2_ip_bases t2,
+            t_ip2_ip_base_names t3
+        WHERE
+            t3.ip2_ipn_id (+) = t1.ip2_ipn_id
+            AND t2.ip2_ipb_id (+) = t3.ip2_ipb_id
+            AND t41.ip2_mbr_ip_base (+) = t2.ip2_ipb_id
+            AND t3.ip2_ipb_id IS NOT NULL
+        UNION
+        SELECT DISTINCT
+            t1.ip2_ipn_id AS ip_name_id,
+            t3.ip2_ipb_id AS ip_base,
+            t2.ip2_ipb_member_code AS ip_member_code,
+            t2.ip2_ipb_code AS ip_base_code,
+            t1.ip2_ipn_name
+            || ' '
+            || t1.ip2_ipn_first_name AS indexed_name,
+            (
+                CASE
+                    WHEN t1.ip2_nsr_id = 2
+                         AND t1.ip2_ipn_internal = 1 THEN 1
+                    ELSE 0
+                END
+            ) AS internal,
+            t2.ip2_ipb_int_role AS role,
+            nvl(lpad(t41.ip2_mbr_soc_code,3,'0'),'099') AS soc_code,
+            t41.ip2_mbr_cc_code AS cc_code,
+            t41.ip2_mbr_rl_code,
+            t41.ip2_mbr_rg_code AS rg_code,
+            t41.ip2_mbr_dt_from,
+            t41.ip2_mbr_dt_to,
+            t41.ip2_mbr_ter_id,
+            t1.ip2_nty_id
+        FROM
+            t_ip2_ip_base_memberships t41,
+            t_ip2_ip_names t1,
+            t_ip2_ip_bases t2,
+            t_ip2_ip_base_names t3,
+            t_ip2_ip_ot_names t4
+        WHERE
+            t4.ip2_otn_ot_ip_name = t1.ip2_ipn_id
+            AND t3.ip2_ipn_id (+) = t4.ip2_otn_ip_name
+            AND t2.ip2_ipb_id (+) = t3.ip2_ipb_id
+            AND t41.ip2_mbr_ip_base (+) = t2.ip2_ipb_id
+    ) t_ipi
+WHERE
+    ip_name_id =:codigoIPI
+    AND (
+        ip2_mbr_dt_from IS NULL
+        OR TO_DATE(ip2_mbr_dt_from,'yyyy-MM-dd hh24:mi:ss') <= TO_DATE(:fechaDevengo,'yyyy-MM-dd hh24:mi:ss')
+    )
+    AND (
+        ip2_mbr_dt_to IS NULL
+        OR TO_DATE(ip2_mbr_dt_to,'yyyy-MM-dd hh24:mi:ss') >= TO_DATE(:fechaDevengo,'yyyy-MM-dd hh24:mi:ss')
+    )
+    AND cc_code IN (:listaClaseCreacion)
+    AND ip2_mbr_rl_code IN (:listaRoles)
+    AND rg_code IN (:listaTipoDerecho)
+ORDER BY
+    DECODE(cc_code,'AV','CC_CODE1','AF','CC_CODE2','AD','CC_CODE3','DW','CC_CODE4','LW','CC_CODE5');
+   
+SELECT * FROM T_DIST.T_DIS_SETTLEMENT;
--- sgae_db.py
+++ sgae_db.py
@@ -17,18 +17,21 @@ dsn = """(DESCRIPTION =
 def conectar(entorno):
     global connection
     global cursor
+    result = False
 
     try:
         connection = cx_Oracle.connect(user=username, password=userpwd, dsn=dsn, encoding=encoding)
         cursor = connection.cursor()
-
+        result = True
     except cx_Oracle.Error as error:
         print("Error al conectar")
         print(error)
 
+    return result
+
 
 def buscar_registro(codigo):
-    '''sql = """select wh.work_header_id, wh.cod_sgae, wh.work_type_id, dc1.doc_date, wc.start_effectivity_date,
+    ''' sql = """select wh.work_header_id, wh.cod_sgae, wh.work_type_id, dc1.doc_date, wc.start_effectivity_date,
     dc1.PERCENTAGE, dc.percentage_d, dc.percentage_f, dc.percentage_l, dc.percentage_m,
     cmp.percentage, cmp.length, cs1.composition_length, cs1.percentage,
     w.distribution_typology_id, w.nationality_id, w.work_length w_work_length,
@@ -51,24 +54,57 @@ def buscar_registro(codigo):
     left join t_aud.version v on v.work_header_id = wh.work_header_id
     where  wh.cod_sgae = :cod """'''
 
-    sql = """SELECT DISTINCT wh.work_header_id, wh.cod_sgae, wh.work_type_id, dc.CONTRACT_HEADER_ID, 
-    dc.DECLARATION_CONTRACT_ID,  dc.PROFESSION_ID, dc.COD_IPI,  dc.PERCENTAGE, dc2.percentage_f, dc2.percentage_l, 
-    dc2.percentage_m, cmp.percentage cmp_percentage, cmp.LENGTH cmp_LENGTH , cs.MOD_DATE fecha_devengo, 
-    cs1.composition_length cs_composition_length, cs1.percentage cs_percentage,  w.distribution_typology_id, 
-    w.nationality_id, w.work_length w_work_length, w.music_length w_music_length, e.work_length w_work_length, 
-    e.music_length e_music_length, v.version_id
-    FROM T_AUD.WORK_HEADER wh 
-    JOIN T_AUD.WORK_CONTRACT wc ON wc.WORK_HEADER_ID = wh.WORK_HEADER_ID 
-    JOIN T_AUD.CONTRACT_HEADER ch ON ch.CONTRACT_HEADER_ID = wc.CONTRACT_HEADER_ID -- AND ch.CONTRACT_TYPE_ID = 1
-    LEFT JOIN T_AUD.DECLARATION_CONTRACT dc ON dc.CONTRACT_HEADER_ID = ch.CONTRACT_HEADER_ID
+    # sql = """SELECT DISTINCT wh.work_header_id, wh.cod_sgae, wh.work_type_id, dc.CONTRACT_HEADER_ID,
+    # dc.DECLARATION_CONTRACT_ID,  dc.PROFESSION_ID, dc.COD_IPI,  dc.PERCENTAGE, dc2.percentage_f, dc2.percentage_l,
+    # dc2.percentage_m, cmp.percentage cmp_percentage, cmp.LENGTH cmp_LENGTH , cs.MOD_DATE fecha_devengo,
+    # cs1.composition_length cs_composition_length, cs1.percentage cs_percentage,  w.distribution_typology_id,
+    # w.nationality_id, w.work_length w_work_length, w.music_length w_music_length, e.work_length w_work_length,
+    # e.music_length e_music_length, v.version_id
+    # FROM T_AUD.WORK_HEADER wh
+    # JOIN T_AUD.WORK_CONTRACT wc ON wc.WORK_HEADER_ID = wh.WORK_HEADER_ID
+    # JOIN T_AUD.CONTRACT_HEADER ch ON ch.CONTRACT_HEADER_ID = wc.CONTRACT_HEADER_ID -- AND ch.CONTRACT_TYPE_ID = 1
+    # LEFT JOIN T_AUD.DECLARATION_CONTRACT dc ON dc.CONTRACT_HEADER_ID = ch.CONTRACT_HEADER_ID
+    # LEFT JOIN T_AUD.DISTRIBUTION_CONTRACT dc2 ON dc2.CONTRACT_HEADER_ID = ch.CONTRACT_HEADER_ID
+    # LEFT JOIN t_aud.composition_cmp cmp on cmp.work_header_id = wh.work_header_id AND wh.WORK_TYPE_ID IN (1,2)
+    # LEFT join t_aud.cue_sheet cs on cs.work_header_id = wh.work_header_id AND wh.WORK_TYPE_ID IN (1,2)
+    # LEFT join T_AUD.composition_cue_sheet cs1 on cs1.work_header_id = wh.work_header_id AND wh.WORK_TYPE_ID IN (1,2)
+    # left join T_AUD.WORK w  on w.work_header_id = wh.work_header_id AND wh.WORK_TYPE_ID = 1
+    # left join t_aud.episode e on e.work_header_id = wh.work_header_id AND wh.WORK_TYPE_ID = 5
+    # left join t_aud.version v on v.work_header_id = wh.work_header_id AND wh.WORK_TYPE_ID IN (4,5)
+    # WHERE wh.COD_SGAE = :COD_SGAE"""
+
+    # sql = """SELECT COD_SGAE, wh.WORK_TYPE_ID,  wc.START_EFFECTIVITY_DATE,  ch.contract_type_id, dc.COD_IPI, count(dc.DECLARATION_CONTRACT_ID) num_contratos, dc.DISTRIBUTION_PART_ID,
+    # sum(nvl(dc.PERCENTAGE, 50)) porcentaje_contrato, dc2.DISTRIBUTION_KEY_ID, nvl(dc2.PERCENTAGE_D, 25) percentage_d, dc2.PERCENTAGE_f, nvl(dc2.PERCENTAGE_L, 50) percentage_l,
+    # nvl(dc2.PERCENTAGE_M, 25) percentaje_m
+    # from t_aud.work_header wh
+    # join t_aud.work_contract wc on wc.work_header_id = wh.work_header_id
+    # join t_aud.contract_header ch on ch.contract_header_id = wc.contract_header_id
+    # LEFT JOIN T_AUD.DECLARATION_CONTRACT dc ON dc.CONTRACT_HEADER_ID = ch.CONTRACT_HEADER_ID
+    # LEFT JOIN T_AUD.DISTRIBUTION_CONTRACT dc2 ON dc2.CONTRACT_HEADER_ID = ch.CONTRACT_HEADER_ID
+    # LEFT join t_aud.cue_sheet cs on cs.work_header_id = wh.work_header_id AND wh.WORK_TYPE_ID IN (1,2)
+    # where wh.cod_sgae= :COD_SGAE
+    # GROUP BY COD_SGAE, wh.WORK_TYPE_ID, wc.START_EFFECTIVITY_DATE, ch.CONTRACT_TYPE_ID, dc.COD_IPI, dc.DISTRIBUTION_PART_ID, dc2.DISTRIBUTION_KEY_ID , dc2.PERCENTAGE_D ,
+    # dc2.PERCENTAGE_D, dc2.PERCENTAGE_F, dc2.PERCENTAGE_L, dc2.PERCENTAGE_M"""
+    sql = """SELECT wh.COD_SGAE, wh.WORK_HEADER_ID, dc.CONTRACT_HEADER_ID, wh.WORK_TYPE_ID, 
+    nvl(w2.DISTRIBUTION_TYPOLOGY_ID, w.DISTRIBUTION_TYPOLOGY_ID) tipologia,  
+    nvl(w2.NATIONALITY_ID, w.NATIONALITY_ID) nacionalidad, wc.START_EFFECTIVITY_DATE,  ch.contract_type_id, 
+    dc.COD_IPI, dc.DECLARATION_CONTRACT_ID, dc.PROFESSION_ID, dc2.DISTRIBUTION_KEY_ID, 
+    dc.PERCENTAGE porcentaje_contrato, nvl(dc2.PERCENTAGE_D, 25) porcentaje_d, 
+    nvl(dc2.PERCENTAGE_L, 50) porcentaje_l, nvl(dc2.PERCENTAGE_M, 25) porcentaje_m
+    from t_aud.work_header wh 
+    join t_aud.work_contract wc on wc.work_header_id = wh.work_header_id 
+    join t_aud.contract_header ch on ch.contract_header_id = wc.contract_header_id 
+    LEFT JOIN T_AUD.DECLARATION_CONTRACT dc ON dc.CONTRACT_HEADER_ID = ch.CONTRACT_HEADER_ID 
     LEFT JOIN T_AUD.DISTRIBUTION_CONTRACT dc2 ON dc2.CONTRACT_HEADER_ID = ch.CONTRACT_HEADER_ID 
-    LEFT JOIN t_aud.composition_cmp cmp on cmp.work_header_id = wh.work_header_id AND wh.WORK_TYPE_ID IN (1,2)
-    LEFT join t_aud.cue_sheet cs on cs.work_header_id = wh.work_header_id AND wh.WORK_TYPE_ID IN (1,2)
-    LEFT join T_AUD.composition_cue_sheet cs1 on cs1.work_header_id = wh.work_header_id AND wh.WORK_TYPE_ID IN (1,2)
-    left join T_AUD.WORK w  on w.work_header_id = wh.work_header_id AND wh.WORK_TYPE_ID = 1
-    left join t_aud.episode e on e.work_header_id = wh.work_header_id AND wh.WORK_TYPE_ID = 5
-    left join t_aud.version v on v.work_header_id = wh.work_header_id AND wh.WORK_TYPE_ID IN (4,5)
-    WHERE wh.COD_SGAE = :COD_SGAE"""
+    LEFT JOIN T_AUD.EPISODE e ON e.WORK_HEADER_ID = wh.WORK_HEADER_ID AND wh.WORK_TYPE_ID = 3 
+    AND e.START_EFFECTIVITY_DATE < to_date('2021-08-18','yyyy-mm-dd')
+    LEFT JOIN T_AUD.WORK_HEADER wh3 ON wh3.WORK_HEADER_ID = e.SERIES_HEADER_ID AND wh.WORK_TYPE_ID = 3
+    LEFT JOIN T_AUD."WORK" w ON w.WORK_HEADER_ID = wh3.WORK_HEADER_ID AND wh.WORK_TYPE_ID = 3
+    LEFT JOIN T_AUD."WORK" w2 ON w2.WORK_HEADER_ID = wh.WORK_HEADER_ID AND wh.WORK_TYPE_ID = 1
+    where wh.cod_sgae= :COD_SGAE and wc.START_EFFECTIVITY_DATE = (
+    SELECT max(wc.START_EFFECTIVITY_DATE) FROM T_AUD.WORK_HEADER wh2
+    JOIN T_AUD.WORK_CONTRACT wc ON wc.WORK_HEADER_ID = wh2.WORK_HEADER_ID 
+    WHERE wh2.cod_sgae = wh.COD_SGAE  and wc.START_EFFECTIVITY_DATE < to_date('2021-08-18','yyyy-mm-dd'))"""
 
     cursor.execute(sql, [codigo])
     
@@ -81,20 +117,26 @@ def buscar_registro(codigo):
 
 
 def guardar(row):
-    with open('resultados_8.csv', 'a', newline='') as file:
-        registros = csv.writer(file, delimiter = ',')
+    with open('resultados_12.csv', 'a', newline='') as file:
+        registros = csv.writer(file, delimiter=',')
         registros.writerow(row)
 
 
 def main():
-    conectar(1)
-    total = 0
+    if not conectar(1):
+        return
+
     encontrados = 0
 
-    with open('ETL_WT01_1.csv') as file:
+    with open('ETL_BROADCASTS_2.csv') as file:
         reader = csv.DictReader(file)
 
-        index = 1
+        headers = ["COD_SGAE", "WORK_HEADER_ID", "CONTRACT_HEADER_ID", "WORK_TYPE_ID", "TIPOLOGIA", "NACIONALIDAD",
+                   "FECHA_INCIO", "CONTRACT_TYPE", "COD_IPI", "DECLARATION_CONTRACT_ID", "PROFESION_ID",
+                   "DISTRIBUTION_KEY_ID", "PORCENTAJE_CONTRATO", "PORCENTAJE_D", "PORCENTAJE_L", "PORCENTAJE_M"]
+
+        guardar(headers)
+
         for row in reader:
             codigo = row['SGAECODE']
             print(f'{reader.line_num} - {codigo}')
@@ -106,8 +148,6 @@ def main():
 
             if len(tmp):
                 encontrados += 1
-
-            index += 1
 
     print(f'{encontrados} de {reader.line_num}')
 
